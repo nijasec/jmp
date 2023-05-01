87c06c6184223d1e3a0b22cfb078441b353e901c
Nijas <57863074+nijasec@users.noreply.github.com>
2023-05-01 13:49:14 +0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 echo "Enter ssh port number"
 read pnumber
 echo "Now your ssh is available online"
-pkill ssh
-killall ssh
+pkill '^ssh$'
+
 sshd
 ssh mydev@ssh-j.com -N -R dev:22:localhost:$pnumber -y &

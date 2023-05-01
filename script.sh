b56ceca690c3347db7112b9193db98acee442f7a
Nijas <57863074+nijasec@users.noreply.github.com>
2023-05-01 14:57:46 +0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,8 +3,7 @@ read pnumber
 echo "Now your ssh is available online"
 pkill '^ssh$'
 sleep 3
-while :
-do
-ssh mydev@ssh-j.com -N -R dev:22:localhost:$pnumber -y 
-sleep 180
-done
+
+ssh mydev@ssh-j.com -N -R dev:22:localhost:$pnumber -y &
+
+

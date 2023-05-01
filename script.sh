60c832f8105234e0776fb32b25db8a4333e89eb1
Nijas <57863074+nijasec@users.noreply.github.com>
2023-05-01 15:01:44 +0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,11 @@
+echo "Enter any username you want to use this for session:"
+read usrname
 echo "Enter ssh port number:"
 read pnumber
 echo "Now your ssh is available online"
 pkill '^ssh$'
 sleep 3
 
-ssh mydev@ssh-j.com -N -R dev:22:localhost:$pnumber -y &
+ssh $usrname@ssh-j.com -N -R dev:22:localhost:$pnumber -y &
 
 
